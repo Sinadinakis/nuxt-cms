5889371dbbe9611865947abc896254d497d88252
Renato Cardoso <re2005@gmail.com>
2019-03-16 18:03:30 +0100

    feat(): add global components

---
+++ components/Globals/Hero.vue
@@ -0,0 +1,20 @@
+<template>
+    <section class="hero is-medium is-primary is-bold">
+        <div class="hero-body">
+            <div class="container">
+                <h1 class="title">
+                    Renatex layout
+                </h1>
+                <h2 class="subtitle">
+                    Hero and footer
+                </h2>
+            </div>
+        </div>
+    </section>
+</template>
+
+<script>
+    export default {
+        name: 'Hero'
+    };
+</script>
