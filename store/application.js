74e5362107aa3f86a0c6085ce02d7d111a1d3ce0
Renato Cardoso <re2005@gmail.com>
2019-03-16 18:07:24 +0100

    refactor(application.js): add config specific properties

--- store/application.js
+++ store/application.js
@@ -1,21 +1,31 @@
 export const state = () => ({
     slugs: undefined,
-    components: []
+    components: [],
+    slugComponents: undefined,
+    is404: false,
+    layouts: undefined,
+    templates: undefined
 });
 
 export const actions = {
     updateSlugs({commit}, value) {
         commit('updateSlugs', value);
     },
-    updateCurrentSlugLayout({commit}, value) {
-        commit('updateSlugs', value);
-    },
-    removeSlug({commit}, slug) {
-        commit('removeSlug', slug);
+    updateCurrentSlugComponents({commit}, value) {
+        commit('updateCurrentSlugComponents', value);
     },
     updateComponents({commit}, value) {
         commit('updateComponents', value);
     },
+    updateIs404({commit}, value) {
+        commit('updateIs404', value);
+    },
+    updateLayouts({commit}, value) {
+        commit('updateLayouts', value);
+    },
+    updateTemplates({commit}, value) {
+        commit('updateTemplates', value);
+    }
 };
 
 export const mutations = {
@@ -24,6 +34,18 @@ export const mutations = {
     },
     updateComponents(state, value) {
         state.components.push(value);
+    },
+    updateCurrentSlugComponents(state, value) {
+        state.slugComponents = value;
+    },
+    updateIs404(state, value) {
+        state.is404 = value;
+    },
+    updateLayouts(state, value) {
+        state.layouts = value;
+    },
+    updateTemplates(state, value) {
+        state.templates = value;
     }
 };
 export const getters = {};
